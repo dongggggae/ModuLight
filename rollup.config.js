142a33deb2de899b761b3e8b09cb6ed0dca82806
hyun <wjdqls149@naver.com>
2025-03-16 19:32:42 +0900

    라이브러리 빌드 시 rollup 스크립트 및 설정 추가, tmp index.ts 파일 추가

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: 'dist/index.js', format: 'cjs' },
+      { file: 'dist/index.esm.js', format: 'esm' },
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
